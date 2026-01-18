7eb83c2165b6757371a6252c8e5b0bdc06e0c74d
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-18 16:37:41 +0800

    Add Erika's and user's designation in the dialogue

--- src/main/java/Erika.java
+++ src/main/java/Erika.java
@@ -5,8 +5,8 @@ public class Erika {
     /** Prints greeting message for the user */
     public static void greetUser() {
         String message = """
-                Hello! I'm Erika
-                What can I do for you?
+                Erika: Hello! I'm Erika
+                       What can I do for you?
                 """;
         System.out.println(message);
     }
@@ -14,7 +14,7 @@ public class Erika {
     /** Prints farewell message for the user */
     public static void bidFarewell() {
         String message = """
-                Bye. Hope to see you again.
+                Erika: Bye. Hope to see you again.
                 """;
         System.out.println(message);
     }
@@ -22,12 +22,13 @@ public class Erika {
     /** Returns string inputted by the user */
     public static String readUserInput() {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("User: ");
         return scanner.nextLine();
     }
 
     /** Prints any message passed through the parameter */
     public static void respondToUser(String message) {
-        System.out.println("\n" + message);
+        System.out.println("\n" + "Erika: " + message + "\n");
     }
 
     /** Maintain conversation until the user inputs "bye" */
