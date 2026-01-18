41a835562129a572b929e308b2372ca051de45ab
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-18 20:29:47 +0800

    Add ToDos, Deadlines, and Events classes

---
+++ src/main/java/Events.java
@@ -0,0 +1,17 @@
+public class Events extends Task {
+    private String begin;
+    private String end;
+
+    public Events(String taskName, String begin, String end) {
+        super(taskName);
+        this.begin = begin;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString()
+                + " (from: " + begin + ")"
+                + " (to: " + end + ")";
+    }
+}
