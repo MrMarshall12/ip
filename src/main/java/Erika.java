506fbf0dad5713853c3ff186660a3e708b0f9f3d
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-18 21:30:03 +0800

    Add addTask method in Erika.java

--- src/main/java/Erika.java
+++ src/main/java/Erika.java
@@ -31,6 +31,39 @@ public class Erika {
         return scanner.nextLine();
     }
 
+    /** Adds task to the list */
+    public void addTask(String formattedMessage) {
+        Task task = null;
+        if (formattedMessage.startsWith("todo")) {
+            String taskName = formattedMessage.replace("todo ", "");
+            task = new ToDos(taskName);
+        } else if (formattedMessage.startsWith("deadline")) {
+            String deadlineContent = formattedMessage.replace("deadline ", "");
+            String[] splitMessage = deadlineContent.split(" /by ");
+            String taskName = splitMessage[0];
+            String deadlineTime = splitMessage[1];
+            task = new Deadlines(taskName, deadlineTime);
+        } else if (formattedMessage.startsWith("event")) {
+            String eventContent = formattedMessage.replace("event ", "");
+            String[] splitMessage = eventContent.split(" /from ");
+            String eventName = splitMessage[0];
+            splitMessage = splitMessage[1].split(" /to ");
+            String startDate = splitMessage[0];
+            String endDate = splitMessage[1];
+            task = new Events(eventName, startDate, endDate);
+        } else {
+            System.out.println("Erika: Sorry, I can't understand you. Please try again.");
+        }
+
+        list.add(task);
+
+        System.out.println("\n" + "Got it. I have added this task:"
+                + "\n"
+                + "\t "
+                + (task != null ? task.toString() : "")
+                + "\n");
+    }
+
     /** Prints any message passed through the parameter */
     public void respondToUser(String message) {
         String formattedMessage = message.strip().toLowerCase();
@@ -52,9 +85,7 @@ public class Erika {
                     + list.getTask(index - 1)
                     + "\n");
         } else {
-            String response = list.add(message);
-            System.out.println("\n" + "Erika: I have added "
-                    + response + " to the list" + "\n");
+            addTask(formattedMessage);
         }
     }
 
