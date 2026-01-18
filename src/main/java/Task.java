935d29aa560ec44c323a1608dafcc23d94131a02
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-18 18:50:12 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+/** A class representing a task */
+public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String toString() {
+        String mark = isDone ? "[X]" : "[ ]";
+        return mark + " " + taskName;
+    }
+}
