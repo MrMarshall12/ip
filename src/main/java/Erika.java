6706f27b89c87f8c42e9cbfe498003909fb8f02f
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-18 15:37:30 +0800

    Add greetUser() method

--- src/main/java/Erika.java
+++ src/main/java/Erika.java
@@ -1,5 +1,14 @@
 /** A class representing the chatbot named Erika */
 public class Erika {
+    /** Prints greeting message for the user */
+    public static void greetUser() {
+        String message = """
+                Hello! I'm Erika
+                What can I do for you?
+                """;
+        System.out.println(message);
+    }
+
     public static void main(String[] args) {
 
     }
