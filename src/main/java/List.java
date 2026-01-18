3d9507ab1339f31354401974715197a888326edc
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-18 18:01:55 +0800

    Fix indexing for add method

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,13 +1,16 @@
 /** A class representing a list */
 public class List {
     private String[] strings;
+    private int index;
 
     public List() {
         strings = new String[100];
+        index = 0;
     }
 
     public void add(String string) {
-        strings[strings.length] = string;
+        strings[index] = string;
+        index++;
     }
 
     public void display() {
