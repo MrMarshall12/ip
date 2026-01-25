dc1b8973ae3243e4d0cac413bb76cf16334ec77f
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-25 18:26:31 +0800

    Update syntax in error message

--- src/main/java/erika/exceptions/EmptyDescriptionException.java
+++ src/main/java/erika/exceptions/EmptyDescriptionException.java
@@ -8,8 +8,8 @@ public class EmptyDescriptionException extends ErikaException {
                 
                 Please ensure you follow either of the syntax below:
                     a. todo <description>
-                    b. deadline <description> /by <deadline>
-                    c. event <description> /from <start> /to <end>
+                    b. deadline <description> /by dd-MM-yyyy HH:mm
+                    c. event <description> /from dd-MM-yyyy HH:mm /to dd-MM-yyyy HH:mm
                 """);
 
     }
