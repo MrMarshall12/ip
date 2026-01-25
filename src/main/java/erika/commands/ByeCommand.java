eefe7dcb3af70804ce202f38b26b6d523249eba1
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-26 07:30:47 +0800

    Move bye logic to ByeCommand

--- src/main/java/erika/commands/ByeCommand.java
+++ src/main/java/erika/commands/ByeCommand.java
@@ -1,9 +1,21 @@
 package erika.commands;
 
+import erika.utilities.TaskList;
+import erika.utilities.Ui;
+
 /** A class representing a bye command */
 public class ByeCommand extends Command {
+    public ByeCommand(String formattedMessage) {
+        super(formattedMessage);
+    }
+
     @Override
     public boolean isBye() {
         return true;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui) {
+        ui.showFarewell();
     }
 }
