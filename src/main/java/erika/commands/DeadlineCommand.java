84ad4e25decaf3d29e1a26ce34c4cdc7b66b004a
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-26 07:13:20 +0800

    Move add deadline logic to DeadlineCommand

--- src/main/java/erika/commands/DeadlineCommand.java
+++ src/main/java/erika/commands/DeadlineCommand.java
@@ -1,9 +1,51 @@
 package erika.commands;
 
+import erika.entities.Deadlines;
+import erika.entities.Task;
+import erika.exceptions.EmptyDeadlineException;
+import erika.exceptions.EmptyDescriptionException;
+import erika.exceptions.ErikaIOException;
+import erika.utilities.TaskList;
+import erika.utilities.Ui;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 /** A class representing a deadline command */
 public class DeadlineCommand extends Command {
+    public DeadlineCommand(String formattedMessage) {
+        super(formattedMessage);
+    }
+
     @Override
     public boolean isDeadline() {
         return true;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui) throws EmptyDescriptionException,
+            EmptyDeadlineException, ErikaIOException, DateTimeParseException {
+        String deadlineContent = super.formattedMessage.toLowerCase().replace("deadline", "").strip();
+        String[] splitMessage = deadlineContent.split("/by");
+        String taskName = splitMessage.length > 0
+                ? splitMessage[0].strip()
+                : "";
+        if (taskName.isEmpty()) {
+            throw new EmptyDescriptionException();
+        }
+
+        String[] splitAroundBy = formattedMessage.split("/by");
+        String deadlineTime = splitAroundBy.length == 2
+                ? splitAroundBy[1].strip()
+                : "";
+        if (deadlineTime.isEmpty()) {
+            throw new EmptyDeadlineException();
+        }
+
+        Task task = new Deadlines(taskName, LocalDateTime.parse(deadlineTime,
+                DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm")));
+        taskList.add(task);
+        ui.showAddedTask(task);
     }
 }
