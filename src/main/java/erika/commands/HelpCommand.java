f6ef8118d8b595f1fdf6569cc97403620ee2ee21
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-26 07:23:22 +0800

    Move help logic to HelpCommand

--- src/main/java/erika/commands/HelpCommand.java
+++ src/main/java/erika/commands/HelpCommand.java
@@ -1,9 +1,21 @@
 package erika.commands;
 
+import erika.utilities.TaskList;
+import erika.utilities.Ui;
+
 /** A class representing a help command */
 public class HelpCommand extends Command {
+    public HelpCommand(String formattedMessage) {
+        super(formattedMessage);
+    }
+
     @Override
     public boolean isHelp() {
         return true;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui) {
+        ui.showHelp();
     }
 }
