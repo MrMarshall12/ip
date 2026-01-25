108321d2dbac487ac22795ac8b89bddfbd1b0506
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-25 12:04:23 +0800

    Create load method in Database

--- src/main/java/erika/utilities/Database.java
+++ src/main/java/erika/utilities/Database.java
@@ -1,6 +1,8 @@
 package erika.utilities;
 
+import erika.entities.Deadlines;
 import erika.entities.Task;
+import erika.entities.ToDos;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -8,6 +10,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Database {
@@ -42,5 +45,30 @@ public class Database {
         } finally {
             fileWriterAppend.close();
         }
+    }
+
+    public ArrayList<Task> load() throws IOException {
+        ArrayList<Task> tasks = new ArrayList<>();
+        Task task = null;
+        boolean isDone = false;
+        while (scanner.hasNextLine()) {
+            String item = scanner.nextLine();
+            String[] items = item.split(",");
+            if (items.length == 3 && items[0].equals("todo")) {
+                isDone = items[1].equals("[X]");
+                task = new ToDos(items[2]);
+            } else if (items.length == 4 && items[0].equals("deadline")) {
+                isDone = items[1].equals("[X]");
+                task = new Deadlines(items[2],  items[3]);
+            } else if (items.length == 5 && items[0].equals("event")) {
+                isDone = items[1].equals("[X]");
+                task = new Deadlines(items[2],  items[3]);
+            } else {
+                break; // Content not in the expected format
+            }
+            task.setDone(isDone);
+            tasks.add(task);
+        }
+        return tasks;
     }
 }
