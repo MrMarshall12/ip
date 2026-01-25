672a36a4fed70b815041586aa416aeea4cf8665b
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-01-25 14:49:37 +0800

    Create ErikaIOException

---
+++ src/main/java/erika/exceptions/ErikaIOException.java
@@ -0,0 +1,8 @@
+package erika.exceptions;
+
+/** A subclass of ErikaException representing ErikaIOException */
+public class ErikaIOException extends ErikaException {
+    public ErikaIOException(String message) {
+        super(message);
+    }
+}
