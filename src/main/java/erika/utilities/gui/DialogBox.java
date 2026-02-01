450154b8918dfe82bd1c4c6fbac5ead95fac77a0
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-02-01 14:16:56 +0800

    Set controller and view for DialogBox

--- src/main/java/erika/utilities/gui/DialogBox.java
+++ src/main/java/erika/utilities/gui/DialogBox.java
@@ -1,7 +1,15 @@
 package erika.utilities.gui;
 
+import java.io.IOException;
+import java.util.Collections;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -10,15 +18,51 @@ import javafx.scene.layout.HBox;
  * A class representing a dialog box.
  */
 public class DialogBox extends HBox {
+    @FXML
     private Label text;
+
+    @FXML
     private ImageView displayPicture;
 
     /**
      * Instantiates an instance of DialogBox.
      */
     public DialogBox(String message, Image avatar) {
-        text = new Label(message);
-        displayPicture = new ImageView(avatar);
-        this.getChildren().addAll(text, displayPicture);
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/DialogBox.fxml"));
+            loader.setRoot(this);
+            loader.setController(this);
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        text.setText(message);
+        displayPicture.setImage(avatar);
+    }
+
+    /**
+     * Flips the dialog box horizontally to make the image appear on the left and the text on the right.
+     */
+    public void flip() {
+        ObservableList<Node> temporary = FXCollections.observableArrayList(this.getChildren());
+        Collections.reverse(temporary);
+        getChildren().setAll(temporary);
+        setAlignment(Pos.TOP_LEFT);
+    }
+
+    /**
+     * Instantiates an dialog box with image on the right and text on the left.
+     */
+    public static DialogBox createUserDialogBox(String message, Image avatar) {
+        return new DialogBox(message, avatar);
+    }
+
+    /**
+     * Instantiates an dialog box with image on the left and text on the right.
+     */
+    public static DialogBox createBotDialogBox(String message, Image avatar) {
+        DialogBox dialogBox = new DialogBox(message, avatar);
+        dialogBox.flip();
+        return dialogBox;
     }
 }
