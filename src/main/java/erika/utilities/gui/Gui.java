7fedde8a87f3b40fe9d69e9a0294efa13daba56b
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-02-01 10:52:39 +0800

    Rename Main to Gui

--- src/main/java/erika/utilities/gui/Gui.java
+++ src/main/java/erika/utilities/gui/Gui.java
@@ -5,7 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Gui extends Application {
 
     @Override
     public void start(Stage stage) {
