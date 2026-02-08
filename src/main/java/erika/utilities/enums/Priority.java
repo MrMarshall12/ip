67e3c78992eaaa9d29750e0196514918a9c72f92
MrMarshall12 <97599540+MrMarshall12@users.noreply.github.com>
2026-02-08 14:48:28 +0800

    Add toString to Priority.java

--- src/main/java/erika/utilities/enums/Priority.java
+++ src/main/java/erika/utilities/enums/Priority.java
@@ -4,7 +4,19 @@ package erika.utilities.enums;
  * An enumerated type representing the priority of a task.
  */
 public enum Priority  {
-    HIGH,
-    MEDIUM,
-    LOW
+    HIGH("high"),
+    MEDIUM( "medium"),
+    LOW("low"),
+    NONE("none");
+
+    private final String priority;
+
+    Priority(String priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "priority: " + priority;
+    }
 }
